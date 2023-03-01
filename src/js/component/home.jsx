c364e6e93417ce09f8b63bc34c6b35299e6f68d0
Ana Pacheco <kornetera@gmail.com>
2023-03-01 07:54:10 +0000

    fet counter and counter to zero

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,23 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+		<div className="container container-fluid d-flex text-center">
+		<div className="counter">
+		<div className="reloj"><i class="fa-regular fa-clock"></i></div>
+		<div className="six">{props.six || 0}</div>
+		<div className="five">{props.five || 0}</div>
+		<div className="four">{props.four || 0}</div>
+		<div className="three">{props.three || 0}</div>
+		<div className="two">{props.two || 0}</div>
+		<div className="one">{props.one || 0}</div>
+ 		 </div>
+  </div>
+	)
 };
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -8,5 +8,38 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
+
+let count = 0;
+setInterval(()=>{
+    let countString = count.toString();
+    let ud = countString[countString.length-1];
+    let dc = countString[countString.length-2];
+    let cn = countString[countString.length-3];
+    let ml = countString[countString.length-4];
+    let dm = countString[countString.length-5];
+    let cm = countString[countString.length-6];
+    
+    count++;
+
+ReactDOM.render(<Home one={ud} two={dc} three={cn} four={ml} five={dm} six={cm}/>, document.querySelector("#app"));
+},1000);
+
+let countToZero = 1000;
+setInterval(()=>{
+    let countString = countToZero.toString();
+    let udz = countString[countString.length-1];
+    let dcz = countString[countString.length-2];
+    let cnz = countString[countString.length-3];
+    let mlz = countString[countString.length-4];
+    let dmz = countString[countString.length-5];
+    let cmz = countString[countString.length-6];
+    
+    countToZero--;
+
+ReactDOM.render(<Home one={udz} two={dcz} three={cnz} four={mlz} five={dmz} six={cmz}/>, document.querySelector("#appb"));
+},1000);
+
+
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+//ReactDOM.render(<Home/>, document.querySelector("#app"));
+
